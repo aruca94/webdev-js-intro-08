e5169817ef1ed1ed67dab5c3ad1e43655eafc357
Josh <aftershock840@gmail.com>
2024-12-28 18:31:04 -0500

    added experimental code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+// let firstArr = [1, 2, 3, 4, 5];
+
+// for( let i = 0; i < firstArr.length; i++){
+//     console.log(firstArr[i]);
+// };
+// console.log(`Next array`);
+// let secondArr = [6, 7, 8, 9, 10, 11, 12];
+
+// for( let arrItems of secondArr) {
+//     console.log(arrItems);
+// };
+// console.log(`Next array`);
+// let thirdArr = ['doe','a','deer','a','female','deer'];
+
+// thirdArr.forEach(function(i) {
+//     console.log(i);
+// });
+// console.log(`Next array`);
+
+// let fourthArr = [1, 2, 3, 4, 5, 6, 7];
+
+// for( let items in fourthArr) {
+//     console.log(items);
+//     items += 5;
+//     console.log(items);
+// };
+
+// I havent researched yet, but in the firefox console I see a distinct difference here from the other loops. This one is returning as a string, not a number.
