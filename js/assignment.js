7705e971e6c6514682b3697b0d7f8a5d2c9d2a94
Josh <aftershock840@gmail.com>
2025-01-06 09:41:30 -0500

    updated functions

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,37 @@ const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+function findSmallestNumber(arr) {
+    for(let i = 0; i < arr.length; i++) {
+    return smallestNumberElement.innerText = Math.min(...arr);
+    };
+};
+
+function findLargestNumber(arr) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i]);
+    };
+    let largest = Math.max(...newArr);
+    console.log(newArr);
+    return largestNumberElement.innerText = largest;
+};
+
+function findAverage() {
+    let sum = 0;
+    for( let i = 0; i < myNumbers.length; i++) {
+        sum += myNumbers[i];
+    };
+    return averageNumberElement.innerText = sum / myNumbers.length;
+};
+
+function render() {
+    findSmallestNumber(myNumbers);
+    findLargestNumber(myNumbers);
+    findAverage();
+};
+
+submissionBtn.addEventListener("click", function(){
+    render();
+});
